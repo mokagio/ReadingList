09f374bc9dbed06fbd0de48d6db44c281452555c
Gio Lodi <giovanni.lodi42@gmail.com>
2020-10-16 13:15:08 +1100

    Generate Swift UI App via Xcode wizard

---
+++ ReadingList/ReadingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadingListApp.swift
+//  ReadingList
+//
+//  Created by Gio on 16/10/20.
+//
+
+import SwiftUI
+
+@main
+struct ReadingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
