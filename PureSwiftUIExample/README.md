bba8f586dfd3beaf5f68d5d4344f561b5f448a72
Gio Lodi <giovanni.lodi42@gmail.com>
2020-10-26 20:28:31 +1100

    Cleanup pure SwiftUI example Tuist files

---
+++ PureSwiftUIExample/README.md
@@ -0,0 +1,1 @@
+Run `tuist generate` to generate the Xcode project using [Tuist](https://tuist.io/)
--- PureSwiftUIExample/Targets/PureSwiftUIExampleKit/Sources/PureSwiftUIExampleKit.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-public final class PureSwiftUIExampleKit {
-    public static func hello() {
-        print("Hello, from your Kit framework")
-    }
-}
